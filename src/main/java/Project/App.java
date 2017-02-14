ba0536ded5c9a0a12b7e496a0dec2cfe22ed5012
Yassin <yassin@incorporated.org>
2017-02-14 15:31:51 -0500

    began the project

---
+++ src/main/java/Project/App.java
@@ -0,0 +1,30 @@
+package Project;
+
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.apache.commons.lang3.StringEscapeUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws UnirestException {
+        String p = "<p> Welcome to my thingy</p>";
+        System.out.println(StringEscapeUtils.escapeHtml4(p));
+
+
+        HttpResponse<JsonNode> response = Unirest.post("https://community-sentiment.p.mashape.com/text/")
+                .header("X-Mashape-Authorization", "<enter API key>")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .header("Accept", "application/json").field("txt", "what is life")
+                .asJson();
+        System.out.println(response.getBody());
+
+
+    }
+}
