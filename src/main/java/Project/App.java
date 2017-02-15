8819091f1768aaa6c5c68d51c9c9c8228be4041a
Yassin <yassin@incorporated.org>
2017-02-14 20:34:30 -0500

    added the gson directory in pom.xml

--- src/main/java/Project/App.java
+++ src/main/java/Project/App.java
@@ -1,12 +1,18 @@
 package Project;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.commons.lang3.StringEscapeUtils;
 import org.json.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 
 /**
  * Hello world!
@@ -19,13 +25,34 @@ public class App
         System.out.println(StringEscapeUtils.escapeHtml4(p));
 
 
-        HttpResponse<JsonNode> response = Unirest.post("https://community-sentiment.p.mashape.com/text/")
-                .header("X-Mashape-Authorization", "jrrH60SvORmshuve3DmPj8Ey8WRap1Kx6hHjsnV1WezguBShda")
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .header("Accept", "application/json").field("txt", "what is life")
-                .asJson();
-        System.out.println(response.getBody());
+//        HttpResponse<JsonNode> response = Unirest.post("https://community-sentiment.p.mashape.com/text/")
+//                .header("X-Mashape-Authorization", "jrrH60SvORmshuve3DmPj8Ey8WRap1Kx6hHjsnV1WezguBShda")
+//                .header("Content-Type", "application/x-www-form-urlencoded")
+//                .header("Accept", "application/json").field("txt", "what is life")
+//                .asJson();
+//        System.out.println(response.getBody());
 
+        Gson gson = new GsonBuilder().create();
+        gson.toJson("Hello",System.out);
+        gson.toJson(123, System.out);
+
+
+        try {
+            Sample();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Didnt work");
+        }
+    }
+
+    public static void Sample() throws IOException {
+        Writer writer = new FileWriter("Output.json");
+        Gson gson = new GsonBuilder().create();
+        gson.toJson("bye",writer);
+        gson.toJson(911, writer);
+        System.out.println();
+        System.out.println(gson);
+        writer.close();
 
     }
 }
--- src/main/java/Project/Math.java
+++ src/main/java/Project/Math.java
@@ -1,9 +1,28 @@
 package Project;
 
+import java.util.Scanner;
+
 /**
  * Created by yassin on 2/14/17.
  */
 public class Math {
 
+    public static void main(String[] args){
+
+        Scanner myScanner = new Scanner(System.in);
+        int myInt = myScanner.nextInt();
+        int mine = myInt/2;
+        for (int i = 1; i <= mine ; i++) {
+            if (i * i == myInt) {
+                i++;
+                System.out.println(i*i);
+                break;
+            }if (i == mine) {
+                System.out.println("i");
+            }
+
+        }
+
+    }
     
 }
